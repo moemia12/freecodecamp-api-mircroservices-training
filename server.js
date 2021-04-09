654b44efc8fb48317e63d1b99be679b8e3ecb503
Mohsin Miah <moemia12@gmail.com>
2021-04-09 16:28:06 +0100

    Completed NPM Package management

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(function(err, req, res, next) {
   }  
 })
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
 
